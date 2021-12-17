3f705bcac28663dffb581a3e977508dacd7d0839
Max Kydanchuk <maxym.kydanchuk@gmail.com>
2021-12-17 15:55:07 +0200

    fix sort and search

--- libs/people/repository.js
+++ libs/people/repository.js
@@ -1,29 +1,28 @@
 import {ObjectId} from "mongodb";
+import {escapeRegExp} from '../../utils/utils.js'
 
 export default class PeopleRepository {
     constructor(repositoryData) {
         this.repositoryData = repositoryData.db('StarWarsDatabase').collection('people');
     }
 
-    escapeRegExp(string) {
-        return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-    }
-
     async getAllPeople(sortBy, sortOrder, searchQuery) {
         let options = {};
 
-        if (sortBy !== undefined && sortOrder !== undefined) {
-            options = {'sort': [sortBy, sortOrder]};
-        }
-        if(searchQuery !== undefined) {
-            options = {"fields.name": {$regex: this.escapeRegExp(searchQuery), $options:"i"}}
+        if (searchQuery !== undefined) {
+            options = {"fields.name": {$regex: escapeRegExp(searchQuery), $options: "i"}}
         }
 
-        if(options.sort) {
-            return  await this.repositoryData.find({}, options).toArray();
-        } else {
-            return await this.repositoryData.find(options).toArray();
+        let cursor = await this.repositoryData.find(options);
+
+        if(sortBy !== undefined && sortOrder !== undefined) {
+            cursor = cursor.sort(sortBy, sortOrder)
         }
+
+        console.log(cursor);
+
+        return await cursor.toArray().then(res => res);
+
     }
 
     async getPerson(id) {
